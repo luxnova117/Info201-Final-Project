c8405f7236b1f960790830a82b679b4a05c6188e
Alex Guo <alexguo117@gmail.com>
2017-11-27 12:09:55 -0800

    added data

---
+++ WDI_data_wrangling.R
@@ -0,0 +1,55 @@
+library(WDI)
+library(dplyr)
+library(httr)
+library(jsonlite)
+
+
+
+dat = WDI(indicator='VC.PKP.TOTL.UN', country = "all", start=2010, end=2011)
+View(dat)
+
+getData <- function(the.indicator, countries, start.year, end.year){
+  if(missing(countries)){
+    countries = "all"
+  }
+  if(missing(start.year)){
+    start.year = 1960
+  }
+  if(missing(end.year)){
+    end.year = 2012
+  }
+  dat <- WDI(indicator = the.indicator, country = countries, start = start.year, end = end.year)
+  return(dat)
+}
+
+indicators <- list("GDP" = "NY.GDP.MKTP.CD",
+                   "GDP_per_capita" = "NY.GDP.PCAP.CD",
+                   "GDP_annual_growth" = "NY.GDP.MKTP.KD.ZG",
+                   "GDP_annual_growth_per_capita" = "NY.GDP.PCAP.CD",
+                   "military_expenditures_pGDP" = "MS.MIL.XPND.GD.ZS", #Military expenditures %GDP
+                   "military_expenditures" = "MS.MIL.XPND.CN",
+                   "military_expenditures_pgovtexpend" = "MS.MIL.XPND.ZS",
+                   "population_total" = "SP.POP.TOTL",
+                   "population_female" = "SP.POP.TOTL.FE.IN",
+                   "population_male" = "SP.POP.TOTL.MA.IN",
+                   "life_expectancy_female" = "SP.DYN.LE00.FE.IN",
+                   "life_expectancy_male" = "SP.DYN.LE00.MA.IN",
+                   "life_expectancy_total" = "SP.DYN.LE00.IN",
+                   "gender_parity_index" = "UIS.LR.AG15T99.GPI",
+                   "refugee_population_asylum" = "SM.POP.REFG",
+                   "refugee_pupulation_origin" = "SM.POP.REFG.OR",
+                   "outbound_mobile_students" = "UIS.OE.56.40510")
+
+countries.response <- GET("https://api.printful.com/countries")
+countries.response.content <- content(countries.response,"text")
+countries.response.data <- fromJSON(countries.response.content)
+countries <- countries.response.data$result
+
+countries.code <- lapply(countries, '[[', 1)
+countries.code <- unlist(countries.code)
+
+countries.name <- lapply(countries, '[[', 2)
+countries.name <- unlist(countries.name)
+
+countries.data <- list(name = countries.name, code = countries.code) # List containing all the countries and their
+                                                                     # 2 digit country codes.
