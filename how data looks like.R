ed22025ddcadda45acffb69fbd79ccfe1daa6af8
emma.l <emmafangliao@gmail.com>
2017-11-27 20:07:01 -0800

    close Decide on what the data visualization will look like and be (histogram, scatterplot....)

--- how data looks like.R
+++ how data looks like.R
@@ -1,3 +1,5 @@
+Rough idea of what data visualization will look like: 
+  
 qqplot allows for comparison of two data sets and does so by comparing their quantiles. 
 Even if both data sets do not contain the same number of values, qqplot automatically does
 interpolation to create extra values for the smaller set. 
@@ -7,3 +9,6 @@ we can also look for data normality with qqnorm(). To evaluate whether you can s
 deviation from the normal, u can add a line with qqline(). 
 > qqnorm( data1$property)
 > qqline( data1$property)
+
+scatterplots will also go well with qqplots if we are finding the correlation between
+two data sets in the world bank data.  
