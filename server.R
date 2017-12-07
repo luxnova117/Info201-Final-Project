4d16d412f0b020da920cade312fcfc15552dc744
unknown <chesdude@gmail.com>
2017-12-06 17:33:56 -0800

    updated

--- server.R
+++ server.R
@@ -18,36 +18,7 @@ g <- list(
    scope = "world"
 )
 
-# p <- plot_geo(migration) %>%
-#   add_trace(
-#     z = ~SM.POP.NETM, color = ~SM.POP.NETM, colors = 'RdYlGn', locations = ~CODE,
-#     text = ~paste0(migration$SM.POP.NETM), type = "choropleth"
-#   ) %>%
-#   layout(
-#     title = 'It Worked?',
-#     geo = g
-#   )
-#
 df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', stringsAsFactors = FALSE)
-
-#Returns a data frame at the given year
-# YEARS GO FROM 1962 TO 2012 JUMPING BY 5 YEARS
-# GetMigrationData <- function(my.year){
-#   migration.data <- getData("SM.POP.NETM") %>%
-#     na.omit() %>%
-#     left_join(df, by = c("country" = "COUNTRY")) %>%
-#     na.omit() %>%
-#     select(CODE, country, SM.POP.NETM, year) %>%
-#     filter(year == my.year)
-#   return(migration.data)
-# }
-# 
-# migration.data <- getData("SM.POP.NETM", start.year = input$year, end.year = input$year) %>% 
-#   na.omit()
-# 
-# data.w.codes <- left_join(migration.data, df, by = c("country" = "COUNTRY")) %>%
-                #na.omit()
-
 
 
 shinyServer(function(input, output) {
@@ -64,6 +35,26 @@ shinyServer(function(input, output) {
     data.w.codes <- left_join(migration.data, df, by = c("country" = "COUNTRY")) %>%
       na.omit()
     return(data.w.codes)
+  })
+  
+  international.migrant.stock.data.for.graph <- reactive({
+    selected.country <- input$con
+    #selected.code <- filter(country.codes, name == selected.country) %>% select(code)
+    migrant.stock.data <- getData("SM.POP.TOTL", countries = selected.country) %>% na.omit()
+    colnames(migrant.stock.data)[3] <- "migrant_stock"
+    migrant.stock.data$migrant_stock <- migrant.stock.data$migrant_stock / 1000000
+    colnames(migrant.stock.data)[3] <- "migrant_stock_millions"
+    return(migrant.stock.data)
+  })
+  
+  net.migration.data.for.graph <- reactive({
+    selected.country <- input$con
+    #selected.code <- filter(country.codes, name == selected.country) %>% select(code)
+    net.data <- getData("SM.POP.NETM", countries = selected.country) %>% na.omit()
+    colnames(net.data)[3] <- "net_migration"
+    net.data$net_migration <- net.data$net_migration / 1000000
+    colnames(net.data)[3] <- "net_migration_millions"
+    return(net.data)
   })
   
   
@@ -87,7 +78,22 @@ shinyServer(function(input, output) {
       )
 
     
-    })
+  })
+  
+  # if net migration chosen, renders graph
+  output$plot2 <- renderPlotly({
+    (ggplot(international.migrant.stock.data.for.graph(), aes(x = ~year, y = ~migrant_stock_millions)) +
+       geom_line()) 
+    # %>%
+    # ggplotly(dynamicTicks = TRUE) %>% layout(xaxis = list(title = "Year"), yaxis = list(title = "Net Migration (millions)")) %>%
+    #   animation_opts(frame = 150, transition = 0, redraw = FALSE)
+  })
+  
+  # if international stock migrant chosen, renders graph
+  output$plot3 <- renderPlotly({
+    (ggplot(net.migration.data.for.graph(), aes(x = ~year, y = ~net_migration_millions)) +
+       geom_line()) 
+  })
   
 })
 
--- ui.R
+++ ui.R
@@ -1,49 +1,73 @@
 library(shiny)
 library(plotly)
 
-source("WDI_data_wrangling.R")
+source("WDI_data_wrangling.R") 
 
 my.ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("net migration",
+      # title
+      titlePanel("Where are people moving to these days? Who is moving and how has it changed over time?"),
+      
+      sidebarLayout(
+        sidebarPanel(
+          
+          # select would let the user choose a country, income type, or some other variable to display
+          selectInput('dataset', "Make these choices into different tabs", 
+                      choices = list(
+                        "Net Immigration" = "SM.POP.NETM",
+                        "College Education Immigration" = "SM.EMI.TERT.ZS"
+                      )
+          ),
+          
+          selectInput('dataset', "Choose Country", 
+                      choices = country.codes$name
+          ),
+          
+          # a slider to change the year dataset displayed
+          # years from 1962 to 2012 that jumps by 5
+          sliderInput(inputId = "year",
+                      label = "Year:",
+                      min = 1962,
+                      max = 2012,
+                      value = 15,
+                      step = 5,
+                      sep = "",
+                      animate = TRUE),
+          
+          # a checkbox input seems fairly appropriate, it would let the user choose 
+          # what they want to view from the data very specifically
+          checkboxInput("value", "label", value = FALSE, width = NULL)
+        ),
+        
+        mainPanel(
+          plotlyOutput("plot")
+        )
+      )
+  ),
   
-  # title
-  titlePanel("Where are people moving to these days? Who is moving and how has it changed over time?"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      
-      # select would let the user choose a country, income type, or some other variable to display
-      selectInput('dataset', "Make these choices into different tabs", 
-                  choices = list(
-                    "Net Immigration" = "SM.POP.NETM",
-                    "College Education Immigration" = "SM.EMI.TERT.ZS"
-                  )
-      ),
-      
-      selectInput('dataset', "Choose Country", 
-                  choices = country.codes$name
-      ),
-      
-      # a slider to change the year dataset displayed
-      # years from 1962 to 2012 that jumps by 5
-      sliderInput(inputId = "year",
-                  label = "Year:",
-                  min = 1962,
-                  max = 2012,
-                  value = 15,
-                  step = 5,
-                  sep = "",
-                  animate = TRUE),
-      
-      # a checkbox input seems fairly appropriate, it would let the user choose 
-      # what they want to view from the data very specifically
-      checkboxInput("value", "label", value = FALSE, width = NULL)
-    ),
-    
-    mainPanel(
-      plotlyOutput("plot")
-    )
+  tabPanel("international migrant stock",
+           titlePanel("Where are people moving to these days? Who is moving and how has it changed over time?"),
+           
+           sidebarLayout(
+             sidebarPanel(
+               # a slider to change the year dataset displayed
+               # years from 1962 to 2012 that jumps by 5
+            
+               #,
+               
+               # a checkbox input seems fairly appropriate, it would let the user choose 
+               # what they want to view from the data very specifically
+               # checkboxGroupInput("con", 'check all the countries you wish to compare', choices = country.codes$name)
+             ),
+             
+             mainPanel(#plotlyOutput("plot"),
+                       plotlyOutput("plot3")
+             )
+           )
   )
   
+)
 )
 
 # Define UI for application
